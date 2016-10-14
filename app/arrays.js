defa5b0c353f13f02a545cf6892b86d85cb3c131
Marika <mballely@gmail.com>
2016-10-13 20:33:11 -0700

    pass most of the array tests

--- app/arrays.js
+++ app/arrays.js
@@ -2,39 +2,78 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    
   },
 
   sum: function(arr) {
+    let sum = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i]
+    }
+    return sum;
   },
 
   remove: function(arr, item) {
+    let newarr = [];
 
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== item) {
+        newarr.push(arr[i]);
+      }
+    }
+
+    return newarr;
   },
 
   removeWithoutCopy: function(arr, item) {
-
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (arr[i] === item) {
+    //     arr.splice(i, 1);
+    //   }
+    //   console.log(arr);
+    //   console.log(arr[i]);
+    // }
+    //
+    // return arr;
   },
 
   append: function(arr, item) {
+    arr.push(item);
 
+    return arr;
   },
 
   truncate: function(arr) {
+    arr.pop();
 
+    return arr;
   },
 
   prepend: function(arr, item) {
+    arr.unshift(item);
 
+    return arr;
   },
 
   curtail: function(arr) {
+    arr.shift();
 
+    return arr;
   },
 
   concat: function(arr1, arr2) {
+    let newarr = [];
 
+    arr1.forEach(function(index) {
+      newarr.push(index);
+    });
+
+    arr2.forEach(function(index) {
+      newarr.push(index);
+    });
+
+    return newarr;
   },
 
   insert: function(arr, item, index) {
@@ -42,7 +81,15 @@ exports.arraysAnswers = {
   },
 
   count: function(arr, item) {
+    let count = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        count ++;
+      }
+    }
+
+    return count;
   },
 
   duplicates: function(arr) {
@@ -50,10 +97,22 @@ exports.arraysAnswers = {
   },
 
   square: function(arr) {
+    let newarr = arr.map(function(i) {
+      return i * i;
+    })
 
+    return newarr;
   },
 
   findAllOccurrences: function(arr, target) {
+    occurrences = [];
 
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        occurrences.push(i);
+      }
+    }
+
+    return occurrences;
   }
 };
